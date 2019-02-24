d1c05b5acf448c339533f44268e2b574ac79f972
Sahil <ssfaruque1@gmail.com>
2019-02-24 15:50:35 -0800

    Removed page table

--- src/rocket_server.c
+++ src/rocket_server.c
@@ -4,16 +4,6 @@
 #include "page.h"
 #include "rocket_server.h"
 
-
-typedef struct PageTable
-{
-    Page* pages;
-    int num_pages;
-} PageTable;
-
-PageTable pageTable = {0, 0};
-
-
 int rocket_server_init(int addr_size)
 {
     static int init = 0;
@@ -21,8 +11,7 @@ int rocket_server_init(int addr_size)
     if(!init)
     {
         init = 1;
-        pageTable.num_pages = (addr_size / PAGE_SIZE);
-        pageTable.pages = (Page*) malloc(addr_size);
+
     }
 
     return 0;
@@ -31,12 +20,6 @@ int rocket_server_init(int addr_size)
 
 int rocket_server_exit()
 {
-    if(pageTable.pages)
-    {
-        free(pageTable.pages);
-        pageTable.pages = NULL;
-        pageTable.num_pages = 0;
-    }
 
     return 0;
 }
