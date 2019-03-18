9b28439033f83b0d08d20548c9f27ffa1e114ca2
sfaruque <sfaruque@ad3.ucdavis.edu@pc24.cs.ucdavis.edu>
2019-03-18 15:48:15 -0700

    Added time stamps

--- tests/segfault_handling/client.c
+++ tests/segfault_handling/client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "rocket_core.h"
 #include "rocket_client.h"
@@ -8,8 +9,24 @@
 const int SHARED_MEM_SIZE = (1 << 30);
 const int NUM_CLIENTS = 4;
 
+
+// https://stackoverflow.com/questions/5141960/get-the-current-time-in-c
+const char* get_local_time()
+{
+    time_t rawtime;
+    struct tm* timeinfo;
+
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+
+    return asctime(timeinfo);
+}
+
+
 int main(int argc, char* argv[])
 { 
+
+
     if(rocket_client_init(SHARED_MEM_SIZE, NUM_CLIENTS, "128.120.211.76") == -1)
     {
         printf("Failed to initialize rocket client!\n");
@@ -22,7 +39,7 @@ int main(int argc, char* argv[])
         int num = 123;
         printf("Client %d is writing\n", client_num);
         rocket_write_addr(get_base_address(), &num, sizeof(int));
-        printf("[OUTPUT] ADDRESS WRITTEN TO %p WITH VALUE %d\n", get_base_address(), *((int*)(get_base_address())));
+        printf("[OUTPUT] ADDRESS WRITTEN TO %p WITH VALUE %d --- %s\n", get_base_address(), *((int*)(get_base_address())), get_local_time());
     }
 
     // writes the value 321 to the base address
@@ -32,7 +49,7 @@ int main(int argc, char* argv[])
         int num = 321;
         printf("Client %d is writing\n", client_num);
         rocket_write_addr(get_base_address(), &num, sizeof(int));
-        printf("[OUTPUT] ADDRESS WRITTEN TO %p WITH VALUE %d\n", get_base_address(), *((int*)(get_base_address())));
+        printf("[OUTPUT] ADDRESS WRITTEN TO %p WITH VALUE %d --- %s\n", get_base_address(), *((int*)(get_base_address())), get_local_time());
     }
 
 
@@ -43,13 +60,13 @@ int main(int argc, char* argv[])
         printf("Client Num: %d\n", client_num);
         int read_num = -1;
         rocket_read_addr(get_base_address(), &read_num, sizeof(int));
-        printf("[OUTPUT] READ VALUE: %d\n", read_num);
+        printf("[OUTPUT] READ VALUE: %d --- %s\n", read_num, get_local_time());
 
         sleep(11);
         printf("Client Num: %d\n", client_num);
         read_num = -1;
         rocket_read_addr(get_base_address(), &read_num, sizeof(int));
-        printf("[OUTPUT] READ VALUE: %d\n", read_num);
+        printf("[OUTPUT] READ VALUE: %d --- %s\n", read_num, get_local_time());
     }
 
     // reads from base address 2 times
@@ -59,13 +76,13 @@ int main(int argc, char* argv[])
         printf("Client Num: %d\n", client_num);
         int read_num = -1;
         rocket_read_addr(get_base_address(), &read_num, sizeof(int));
-        printf("[OUTPUT] READ VALUE: %d\n", read_num);
+        printf("[OUTPUT] READ VALUE: %d --- %s\n", read_num, get_local_time());
 
         sleep(11);
         printf("Client Num: %d\n", client_num);
         read_num = -1;
         rocket_read_addr(get_base_address(), &read_num, sizeof(int));
-        printf("[OUTPUT] READ VALUE: %d\n", read_num);
+        printf("[OUTPUT] READ VALUE: %d --- %s\n", read_num, get_local_time());
     }
     
     while(1);
